0b19cafa9097d4d3876cf61592825c5eac6fa14f
bansalnmn <bansalnmn@gmail.com>
2024-06-15 15:05:09 +0530

    update browser

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ app.get("/", (req, res) => {
 
 const scrapeMedium = async (query) => {
   const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/google-chrome-stable',
+    headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    headless: true
   });
   const page = await browser.newPage();
 
