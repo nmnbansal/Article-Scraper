09f9d74d32dfaea166514fb384c4af8701051ffe
bansalnmn <bansalnmn@gmail.com>
2024-06-15 14:56:19 +0530

    update backend

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ app.get("/", (req, res) => {
 });
 
 const scrapeMedium = async (query) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome-stable',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true
+  });
   const page = await browser.newPage();
 
   try {
