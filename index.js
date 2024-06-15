8a669e1d0e3fee6fd2cdedbf73fdd16aec858915
Naman Bansal <59911232+nmnbansal@users.noreply.github.com>
2024-06-15 15:14:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
 
 const scrapeMedium = async (query) => {
   const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome-stable',
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
